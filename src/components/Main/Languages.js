2fd9d34d73b3bdf87a8ded9546bcf96490a89749
TylerRWiddison <tyler.rwiddison@gmail.com>
2021-12-07 14:59:46 -0700

    saved again

---
+++ src/components/Main/Languages.js
@@ -0,0 +1,48 @@
+import { Fragment } from "react";
+
+// faReact, faJs, faCss3, faHtml5
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faReact,
+  faJs,
+  faHtml5,
+  faCss3Alt,
+} from "@fortawesome/free-brands-svg-icons";
+
+import "./Languages.css";
+
+const Languages = () => {
+  return (
+    <Fragment>
+      <h1 className='language-header'>Languages</h1>
+      <div className="language-container color">
+        <section>
+          <span>
+            <FontAwesomeIcon icon={faReact} size="5x" />
+          </span>
+          <h1>React JS</h1>
+        </section>
+        <section>
+          <span>
+            <FontAwesomeIcon icon={faJs} size="5x" />
+          </span>
+          <h1>Javascript</h1>
+        </section>
+        <section>
+          <span>
+            <FontAwesomeIcon icon={faCss3Alt} size="5x" />
+          </span>
+          <h1>CSS3</h1>
+        </section>
+        <section>
+          <span>
+            <FontAwesomeIcon icon={faHtml5} size="5x" />
+          </span>
+          <h1>HTML5</h1>
+        </section>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Languages;
